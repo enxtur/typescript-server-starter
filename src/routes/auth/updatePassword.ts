c39496b309a36ffdaa029c7dfc9e67eaeed1ae12
enxtur <enxtur.mon@gmail.com>
2022-10-13 20:55:02 +0800

    improves auth/updatePassword

--- src/routes/auth/updatePassword.ts
+++ src/routes/auth/updatePassword.ts
@@ -1,22 +1,29 @@
 import { Response, Router } from "express";
 import joi from "joi";
-import { UnauthorizedError } from "../../libs/error-handler";
+import { ValidationError } from "../../libs/error-handler";
 import { authorization } from "../../middlewares/authorization";
 import { catcher } from "../../middlewares/catcher";
 import { Request, user } from "../../middlewares/user";
 import { validate } from "../../middlewares/validator";
 
 const schema = joi.object({
-  password: joi.string().required(),
+  password   : joi.string().required(),
+  newPassword: joi.string().min(4).alphanum().required(),
 });
 
 export const updatePassword = Router();
 
-
 updatePassword.put("/password", authorization, user, validate(schema), catcher(async (req: Request, res: Response) => {
   if (!req.user) {
-    throw new UnauthorizedError("Unauthorized");
-  } 
-  await req.user.updatePassword(req.body.password);
+    throw new Error("User not initialized");
+  }
+
+  const password: string = req.body.password;
+  if (req.user.validatePassword(password)) {
+    throw new ValidationError("Invalid password");
+  }
+  
+  const newPassword: string = req.body.newPassword;
+  await req.user.updatePassword(newPassword);
   res.json({});
 }));
